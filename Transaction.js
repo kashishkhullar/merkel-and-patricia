8b804993c49049510382d756077e0818dac2ce9b
Kashish Khullar <kkhullar7@gmail.com>
2020-03-02 16:14:32 +0530

    created basic patricia tree and transaciton

---
+++ Transaction.js
@@ -0,0 +1,15 @@
+class Transaction {
+    constructor(to, from, amount) {
+        this.to = to;
+        this.from = from;
+        this.amount = amount;
+        this.hash = "Random_String";
+        this.signature = "Random_String";
+    }
+
+    //TODO: Add toString function
+}
+
+let trx = new Transaction(1, 2, 3);
+
+console.log(trx);
